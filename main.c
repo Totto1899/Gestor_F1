2ade72521d6fc7c3fdb3cb5175199ba1a835208e
martucg <martinacarrizo992@gmail.com>
2026-05-27 14:29:31 -0300

    tpiloto

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "library.h"
 
 int main()
 {
-    printf("hola");
+    tPiloto pilotos[]={{1, "martinez", "españa", 3, 12,'A', 11223030},
+                        {2, "gonzalez", "argentino",4,5,'A', 123224}};
+
+    mostrar(pilotos,2);
     return 0;
 }
+
